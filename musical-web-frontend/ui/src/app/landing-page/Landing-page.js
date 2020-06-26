67efbabc9c1754e45eebb4d452620a17be970b49
Bui Quang Ha <buiquangha250199@gmail.com>
2020-06-26 15:13:15 +0700

    change color

--- musical-web-frontend/ui/src/app/landing-page/Landing-page.js
+++ musical-web-frontend/ui/src/app/landing-page/Landing-page.js
@@ -10,12 +10,12 @@ const LandingPage = () => {
     return (
         <div>
             <div className="bg-local w-full h-screen">
-                <div className='relative w-full h-full z-10 bg-gray-100 bg-opacity-25'>
+                <div className='relative w-full h-full z-10 bg-gray-500 bg-opacity-25'>
                     <Navbar />
 
                     <div className="w-full rounded mt-64">
-                        <div className="text-large green-text font-bold">Sign up for free</div>
-                        <div className="text-3xl green-text font-bold text-green-500 pt-4">Enjoy endless ad-free music. Customize your music.</div>
+                        <div className="text-large text-blue-700 font-bold">Sign up for free</div>
+                        <div className="text-3xl text-blue-700 font-bold pt-4">Enjoy endless ad-free music. Customize your music.</div>
                         <a href="/login">
                             <button class="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded mt-4">
                             TRY IT NOW <i className="ion-radio-waves"></i>
